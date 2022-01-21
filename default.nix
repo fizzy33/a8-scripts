915dad1647bfdd9882b8d7b6ae6770d9cade5919
zimbatm <zimbatm@zimbatm.com>
2022-01-21 11:36:22 +0100

    nix: start wrapping the Haxe programs

--- default.nix
+++ default.nix
@@ -4,4 +4,36 @@
 , lib ? nixpkgs.lib
 , src ? lib.cleanSource ./.
 }:
-{ }
+{
+  # Encode a common pattern for those Haxe scripts.
+  a8-scripts = nixpkgs.stdenv.mkDerivation {
+    name = "a8-scripts";
+
+    inherit src;
+
+    nativeBuildInputs = [
+      nixpkgs.makeWrapper
+    ];
+
+    buildInputs = [
+      nixpkgs.python3
+    ];
+
+    installPhase = ''
+      mkdir -p $out/bin
+
+      # Copy all the source into $out/bin
+      cp -r ./* $out/bin/
+
+      # Remove unneeded Nix code
+      rm $out/bin/*.{nix,lock}
+
+      # Add runtime dependency.
+      #
+      # Currently breaks the launcher internal logic
+      wrapProgram $out/bin/a8-launcher.py \
+        --argv0 '$0' \
+        --prefix PATH : ${lib.makeBinPath [ nixpkgs.openjdk11_headless ]}
+    '';
+  };
+}
