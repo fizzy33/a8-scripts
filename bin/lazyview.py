88486b64cc9ba3786f94d0dd3788c453f94db6df
glen@stella <glen@accur8software.com>
2023-07-15 09:28:21 -0400

    rsnapshot switch to use sync based workflow

---
+++ bin/lazyview.py
@@ -0,0 +1,38 @@
+
+
+
+class LazyView:
+
+    def __init__(self, iterableFn):
+        self.iterableFn = iterableFn
+        self.memoized = None
+
+    def filter(self, fn):
+        return LazyView(lambda: filter(fn, self.iterableFn()))
+
+    def map(self, fn):
+        return LazyView(lambda: map(fn, self.iterableFn()))
+
+    def flatmap(self, fn):
+        return LazyView(lambda: map(fn, self.iterableFn()))
+
+    def to_list(self) -> list:
+        if self.memoized is None:
+            self.memoized = list(self.iterableFn())
+        return self.memoized
+
+    def is_empty(self) -> bool:
+        return len(self.to_list()) == 0
+
+    def non_empty(self) -> bool:
+        return not self.is_empty()
+
+    def foreach(self, fn) -> None:
+        for x in self.to_list():
+            fn(x)
+
+    def mk_str(self, sep) -> str:
+        return sep.join(self.map(str).to_list())
+
+def from_iter(iterable) -> list:
+    return LazyView(lambda: iterable)
---
+++ bin/rotation.py
@@ -0,0 +1,53 @@
+
+import datetime
+from pathlib import Path
+import lazyview
+
+class Rotation:
+
+    def __init__(self, serviceName: str, stanzaName: str, rotationName: str, timeDelta: datetime.timedelta):
+        self.serviceName = serviceName
+        self.stanzaName = stanzaName
+        self.rotationName = rotationName
+        self.timeDelta = timeDelta
+        self.now = datetime.datetime.now()
+        self.lastRunFile = Path(f"~/.state/{self.serviceName}-{self.stanzaName}-runs/last-{self.rotationName}").expanduser()
+        
+        self.loadLastRotation()
+        # print(f"lastRotation for {self.name} is {self.lastRun}")
+        if self.lastRun is None:
+            self.needsRotation = True
+            self.nextRotation = None
+        else:
+            self.nextRotation = self.lastRun + self.timeDelta
+            self.needsRotation = self.now >= self.nextRotation
+
+    def loadLastRotation(self) -> datetime.datetime:
+        self.lastRun: Optional[datetime.datetime] = None
+        if self.lastRunFile.exists():
+            # try:
+                datetimeStr = self.lastRunFile.read_text()
+                self.lastRun = datetime.datetime.strptime(datetimeStr, '%Y-%m-%d %H:%M:%S.%f')
+            # except:
+                # lastRun = None
+
+    def rotationCompletedSuccessfully(self):
+        if self.needsRotation:
+            print(f"writing {self.lastRunFile}")
+            self.lastRunFile.parent.mkdir(parents=True, exist_ok=True)
+            self.lastRunFile.write_text(str(self.now))
+
+def load_rsnapshot_rotations(stanzaName: str) -> lazyview.LazyView:
+    return lazyview.from_iter([
+        # systemd runs hourly so this will catch it
+        Rotation("rsnapshot", stanzaName, "hourly", datetime.timedelta(minutes=50)),
+        Rotation("rsnapshot", stanzaName, "daily", datetime.timedelta(days=1)),
+        Rotation("rsnapshot", stanzaName, "weekly", datetime.timedelta(days=7)),
+        Rotation("rsnapshot", stanzaName, "monthly", datetime.timedelta(days=30)),
+        Rotation("rsnapshot", stanzaName, "yearly", datetime.timedelta(days=365)),
+    ])
+
+
+def load_needed_rsnapshot_rotations(stanzaName: str) -> lazyview.LazyView:
+    return load_rsnapshot_rotations(stanzaName).filter(lambda r: r.needsRotation)
+
