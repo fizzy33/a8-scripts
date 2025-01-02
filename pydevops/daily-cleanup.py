dc25c335f629663e593ab644f120613cbe954ddf
glen@stella <glen@accur8software.com>
2025-01-02 12:34:03 -0500

    fixes to get the latest a8-scripts version bumped

--- pydevops/daily-cleanup.py
+++ pydevops/daily-cleanup.py
@@ -3,7 +3,6 @@
 from pyparsing import ParseResults
 import model
 from model import ChangeTheWorldServices, LogArchiver, YearMonthDayDirs, YearMonthDayNestedDirs, PostgresLogArchiver, DryRunServices, Process, CleanerUpper
-from pydevops import model
 from util import Path, logger
 from datetime import timedelta
 import argparse
